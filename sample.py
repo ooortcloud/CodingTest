dd15f75b39b29329fc47847321368ca466f6d75f
ooortcloud <110080440+ooortcloud@users.noreply.github.com>
2024-08-26 17:22:48 +0900

    update: 24-08-26

--- sample.py
+++ sample.py
@@ -1,37 +1,18 @@
-from collections import deque
+n, k = map(int, input("n, k").split())
 
-n, m = map(int, input("n, m 입력").split())
+listA = list(map(int, input().split()))
+listB = list(map(int, input().split()))
 
-graph = []
-for _ in range(n):
-    graph.append( list(map(int, input())) )
+# A는 오름차순 정렬.
+# B는 내림차순 정렬.
+listA.sort()
+listB.sort(reverse=True)
 
-# 컨트롤러 생성(상하좌우)
-moveList = [(-1, 0), (1, 0), (0,-1), (0,1)] 
+for i in range(k):
+    listA[i], listB[i] = listB[i], listA[i]
 
-def dfs(startX, startY):
-    
-    queue = deque([])
-    queue.append((startX, startY))
+sum = 0
+for num in listA:
+    sum += int(num)
 
-    while queue:
-
-        currentX, currentY = queue.popleft()
-
-        # 상하좌우 이동
-        for moveX, moveY in moveList:
-            nextX = currentX + moveX
-            nextY = currentY + moveY
-
-            # 장외의 경우는 스킵
-            if nextX < 0 or nextX >= n or nextY < 0 or nextY >= m:
-                continue
-
-            # 괴물이 없으면 이동
-            if graph[nextX][nextY] == 1:
-                graph[nextX][nextY] += graph[currentX][currentY]  # 현재 값에 이전 누적 카운트를 추가시킴 (헨델과 그레텔)
-                queue.append( (nextX, nextY) ) 
-
-    return graph[n-1][m-1]  # 미로의 출구는 반드시 (n, m)에 있다고 문제에 정의됨
-        
-print( dfs(0, 0) ) # 현재 위치를 기준으로 탐색 시작. 문제에서는 시작 위치를 (1,1)으로 정의했으나, 컴퓨터 상에서는 (0,0)으로 보아야 함
+print(sum)
