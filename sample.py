d39dd7c5a8524e427b0bd55dad2b461acc7ad2d5
ooortcloud <110080440+ooortcloud@users.noreply.github.com>
2024-09-03 21:41:51 +0900

    update: 24-09-03

--- sample.py
+++ sample.py
@@ -1,30 +1,22 @@
-import sys
-
 n, m = map(int, input().split())
 
-riceCakeHeightList = list(map(int, sys.stdin.readline().split()))
-# riceCakeHeightList.sort(reverse=True) # 이진탐색을 위해 내림차순 정렬
+coinList = []
+for _ in range(n):
+    coinList.append(int(input()))
 
+dpTable = [99999] * 10001
+# 동전 종류 중 0에 해당하는 동전은 없음
+dpTable[0] = 0
 
-startHeight = min(riceCakeHeightList)
-endHeight = max(riceCakeHeightList)
-result = -1  # 조건을 만족하는 떡 길이
-while startHeight <= endHeight:
+# coin 종류마다 반복시킴. coinList은 정렬될 필요가 없음. 어차피 최소값으로 갱신할 거라서.
+for coin in coinList:
+    # 현재 시작 coin부터 목표 금액만큼만 순차반복시킴.
+    for i in range(coin, m+1):
+        # 현재 DP table에 저장된 값과 이전 인덱스에서 현재 coin을 1회 사용하여 만든 목표금액 중 더 작은 횟수로 갱신시킴.
+        dpTable[i] = min(dpTable[i], dpTable[i-coin] + 1)
 
-    midHeight = (startHeight + endHeight) // 2
-
-    total = 0  # 남는 떡의 최종 길이    
-    # 전체 떡들에 대해서 커팅식
-    for selectedHeight in riceCakeHeightList:
-        if(midHeight < selectedHeight):
-            total += (selectedHeight - midHeight)
-
-    # 필요 이상으로 남는 떡의 길이가 긴 경우, 자르는 떡의 길이가 지금보다 긴 쪽으로 탐색 진행
-    if total >= m:
-        startHeight = midHeight + 1
-        result = midHeight
-    # 남는 떡의 길이가 부족한 경우, 자르는 떡의 길이가 지금보다 짧은 쪽으로 탐색 진행
-    else:
-        endHeight = midHeight - 1
-
-print(result)
+# 보유한 코인으로 목표 금액을 도달할 수 없는 경우 -1을 출력
+if dpTable[m] == 99999:
+    print(-1)
+else:
+    print(dpTable[m])
