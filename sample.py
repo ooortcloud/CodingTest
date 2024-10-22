dc4e049ab0b8076c33564ba1e3667b03be5eb5c4
ooortcloud <110080440+ooortcloud@users.noreply.github.com>
2024-10-22 22:40:50 +0900

    feat:24-10-22

--- sample.py
+++ sample.py
@@ -1,35 +1,15 @@
-def check(src, dst):
-    global graph
-
-    searchList = []
-    searchList.append(src)
-    
-    # 중복 탐색 방지를 위한 방문 리스트 작성
-    visitList = []
-
-    while searchList:
-        next = searchList.pop()
-        visitList.append(next)
-        for nodeList in graph[next]:
-            for index, val in enumerate(nodeList):
-                if (val == 1) and (index not in visitList):
-                    if index == dst:
-                        return 1
-                    else:
-                        searchList.append(index)            
-
-    return 0
-
 n = int(input())
 
-graph = [[]*n for _ in range(n)]
+graph = []
 for _ in range(n):
     graph.append(list(map(int, input().split())))
 
+# 플로이드 워셜 알고리즘 활용
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if (graph[i][k] + graph[k][j] == 2):
+                graph[i][j] = 1
+
 for i in range(n):
-    for j in range(n):
-        if i == j:
-            print(1, end=" ")
-        else:
-            print(check(i, j), end=" ")
-    print()
+    print(' '.join(map(str, graph[i])))
